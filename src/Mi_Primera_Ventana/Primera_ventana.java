c2ca9f28c0af658f7540142e0115bbdfca3cb7c5
KevinLoayzaS <kevin28042001@gmail.com>
2023-09-25 09:23:57 +0200

    Desarrollo_Interfaces

---
+++ src/Mi_Primera_Ventana/Primera_ventana.java
@@ -0,0 +1,12 @@
+package Mi_Primera_Ventana;
+
+import javax.swing.*;
+
+public class Primera_ventana {
+    public static void main(String[] args) {
+        JFrame f = new JFrame();
+        f.setBounds(10,10,300,200);
+        f.setVisible(true);
+
+    }
+}
